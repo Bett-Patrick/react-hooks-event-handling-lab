9564049df7c06561ad4e2346bb70deeb2fed59ba
Bett <bettpatrick080@gmail.com>
2024-02-07 14:34:33 +0300

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function handleOnFocus(){
+    console.log("Good!")
+}
+
+function handleOnBlur(){
+    console.log("Hey! Eyes on me!")
+}
+
+function EyesOnMe(){
+    return <div>
+        <button onFocus={handleOnFocus} onBlur={handleOnBlur}>Eyes on me</button>
+    </div>
+}
+
+export default EyesOnMe;
